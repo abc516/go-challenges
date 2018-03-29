f871f15b7823bc82abb00671dbe5c22ee494f73a
Anton Cataldi <antoncataldi@gmail.com>
2018-03-28 21:51:52 -0400

    add python reference file from https://ide.geeksforgeeks.org/index.php

--- 8/sudoku.go
+++ 8/sudoku.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	i:= 1
+	for i=1; i <100 ;i++  {
+		if i % 3 == 0 {
+			fmt.Println("Fizz")
+			continue
+		}
+		if i % 5 == 0 {
+			fmt.Println("Buzz")
+			continue
+		} else {
+		    fmt.Println(strconv.Itoa(i))
+		}
+
+	}
+}
