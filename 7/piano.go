0339d5063fe04c281215d655c2695470e22a80bc
Anton Cataldi <antoncataldi@gmail.com>
2018-04-30 23:54:53 -0400

    add outline for app development and basic apk

--- 7/piano.go
+++ 7/piano.go
@@ -11,3 +11,13 @@ func main()  {
 	print(x)
 }
 
+//steps to get up and running
+
+//phase 0.5 get the android app running with an image
+
+//phase 1, on android app, when you hit the image, a sound plays
+
+//phase 1.5 two images are on the screen, a different sound will play when each image is hit
+
+//phase 2 multiple images, multiple sounds
+
